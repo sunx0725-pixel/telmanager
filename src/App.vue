c0f2a2cc52c932fcdde0fbc2ead11b0124439718
JohnnySunny <sunx0725@gmail.com>
2026-05-12 00:20:36 +0800

    Please enter the commit message for your changes. Lines starting

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { onLaunch, onShow, onHide } from '@dcloudio/uni-app'
+
+onLaunch(() => {
+  console.log('APP Launch')
+})
+
+onShow(() => {
+  console.log('APP Show')
+})
+
+onHide(() => {
+  console.log('APP Hide')
+})
+</script>
+
+<style lang="scss">
+@import './styles/index.scss';
+</style>
---
+++ src/shims-uni.d.ts
@@ -0,0 +1,14 @@
+export {}
+
+declare module 'vue' {
+  type Hooks = App.AppInstance & Page.PageInstance
+  interface ComponentCustomOptions extends Hooks {}
+}
+
+declare global {
+  namespace UniApp {
+    interface UniApp {
+      $u?: any
+    }
+  }
+}
